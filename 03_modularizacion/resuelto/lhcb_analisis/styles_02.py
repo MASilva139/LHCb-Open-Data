b1060f5ed03733a1ab8931a3eb367145be3b8558
MASilva139 <mariosva139@hotmail.com>
2026-07-24 11:56:59 -0600

    LHCb:13-(Design:03-Mod:01-UpROOT:06)

---
+++ 03_modularizacion/resuelto/lhcb_analisis/styles_02.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+from .config_02 import (
+    DARK_BACKGROUND,
+    LIGHT_TEXT,
+    OUTPUT_DIR
+)
+
+class PlotStyle:
+    @staticmethod
+    def apply_dark_axes_style(fig, ax, title, xlabel, ylabel):
+        ax.set_facecolor(DARK_BACKGROUND)
+        fig.patch.set_facecolor(DARK_BACKGROUND)
+        ax.set_title(title, color=LIGHT_TEXT, pad=12, fontweight="bold")
+        ax.set_xlabel(xlabel, color=LIGHT_TEXT)
+        ax.set_ylabel(ylabel, color=LIGHT_TEXT)
+        ax.tick_params(axis="both", colors=LIGHT_TEXT)
+        for spine in ax.spines.values():
+            spine.set_edgecolor(LIGHT_TEXT)
+
+    @staticmethod
+    def add_dark_colorbar(fig, ax, image, label="Frecuencia"):
+        colorbar = fig.colorbar(image, ax=ax)
+        colorbar.set_label(label, color=LIGHT_TEXT)
+        colorbar.ax.yaxis.set_tick_params(color=LIGHT_TEXT)
+        plt.setp(colorbar.ax.yaxis.get_ticklabels(), color=LIGHT_TEXT)
+        colorbar.outline.set_edgecolor(LIGHT_TEXT)
+        return colorbar
+    
+    @staticmethod
+    def save_fig(fig, filename, output_dir=OUTPUT_DIR, dpi=500):
+        output_dir =  Path(output_dir)
+        output_dir.mkdir(exist_ok=True)
+        output_path = output_dir / f'{filename}.png'
+        fig.savefig(
+            output_path,
+            dpi =  dpi,
+            bbox_inches = 'tight',
+            facecolor = fig.get_facecolor()
+        )
+        print(f'Figura guardada en: {output_path}')
